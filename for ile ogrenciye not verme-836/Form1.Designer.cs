ad843ea75082ce0bbabdab466161c12eaf746013
ilayda yalçın <146716704+ilaydayacin@users.noreply.github.com>
2023-10-24 15:09:40 +0300

    Add files via upload

---
+++ for ile ogrenciye not verme-836/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace for_ile_ogrenciye_not_verme_836
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNotVer = new System.Windows.Forms.Button();
+            this.lbNotlar = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnNotVer
+            // 
+            this.btnNotVer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnNotVer.Location = new System.Drawing.Point(25, 30);
+            this.btnNotVer.Name = "btnNotVer";
+            this.btnNotVer.Size = new System.Drawing.Size(197, 82);
+            this.btnNotVer.TabIndex = 0;
+            this.btnNotVer.Text = "Not ver";
+            this.btnNotVer.UseVisualStyleBackColor = true;
+            this.btnNotVer.Click += new System.EventHandler(this.btnNotVer_Click);
+            // 
+            // lbNotlar
+            // 
+            this.lbNotlar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbNotlar.FormattingEnabled = true;
+            this.lbNotlar.ItemHeight = 25;
+            this.lbNotlar.Location = new System.Drawing.Point(228, 30);
+            this.lbNotlar.Name = "lbNotlar";
+            this.lbNotlar.Size = new System.Drawing.Size(261, 404);
+            this.lbNotlar.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 450);
+            this.Controls.Add(this.lbNotlar);
+            this.Controls.Add(this.btnNotVer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNotVer;
+        private System.Windows.Forms.ListBox lbNotlar;
+    }
+}
+
---
+++ for ile ogrenciye not verme-836/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_ile_ogrenciye_not_verme_836
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNotVer_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            lbNotlar.Items.Clear();
+            for (int i = 1; i < 16; i++)
+            {
+                int not = rnd.Next(0, 100);
+                lbNotlar.Items.Add(" + "+not.ToString());
+            }
+        }
+    }
+}
